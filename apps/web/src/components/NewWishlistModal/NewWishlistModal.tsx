515dd83645a570a63c797ca59f1cf2c76496e0dd
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-08-04 23:28:48 +0300

    feat: add `NewWishlistModal` component

---
+++ apps/web/src/components/NewWishlistModal/NewWishlistModal.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import { Button, Checkbox, Input, Modal } from "@wishlify/ui";
+
+import { useForm } from "react-hook-form";
+import create from "zustand";
+
+export type CreateWishlistForm = {
+  name: string;
+  private: boolean;
+};
+
+export type NewWishlistModalState = {
+  isNewWishlistModalOpen: boolean;
+  openNewWishlistModal: () => void;
+  closeNewWishlistModal: () => void;
+};
+
+export const useNewWishlistModal = create<NewWishlistModalState>((set) => ({
+  isNewWishlistModalOpen: false,
+  openNewWishlistModal: () => set({ isNewWishlistModalOpen: true }),
+  closeNewWishlistModal: () => set({ isNewWishlistModalOpen: false }),
+}));
+
+export type NewWishlistModalProps = {
+  onSubmit: (data: CreateWishlistForm) => void;
+};
+
+export const NewWishlistModal: React.FC<NewWishlistModalProps> = ({
+  onSubmit,
+}) => {
+  const { isNewWishlistModalOpen, closeNewWishlistModal } =
+    useNewWishlistModal();
+
+  const form = useForm<CreateWishlistForm>({
+    defaultValues: {
+      private: false,
+    },
+  });
+
+  return (
+    <Modal
+      isOpen={isNewWishlistModalOpen}
+      onClose={closeNewWishlistModal}
+      title="Новый вишлист"
+      className="w-full md:w-[400px]"
+      beforeClose={() => form.reset()}
+    >
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 items-start mt-4"
+      >
+        <Input
+          type="text"
+          placeholder="Awesome wishlist name"
+          error={!!form.formState.errors.name}
+          helperText={form.formState.errors.name?.message}
+          {...form.register("name", { required: "Обязательное поле" })}
+        />
+        <Checkbox
+          checked={form.getValues("private")}
+          onCheckedChange={(checked) => form.setValue("private", checked)}
+          label="Сделать вишлист приватным"
+        />
+        <Button type="submit">Создать</Button>
+      </form>
+    </Modal>
+  );
+};
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,13 +1,17 @@
 import { GetServerSideProps } from "next";
 
-import { Button, Input } from "@wishlify/ui";
+import { Button } from "@wishlify/ui";
 
 import { unstable_getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
-import { useForm } from "react-hook-form";
 
 import { trpc } from "@/utils/trpc";
 
+import {
+  CreateWishlistForm,
+  NewWishlistModal,
+  useNewWishlistModal,
+} from "@/components/NewWishlistModal";
 import { WishlistCard } from "@/components/WishlistCard";
 import { DashboardLayout } from "@/layouts/DashboardLayout";
 
@@ -15,45 +19,29 @@ import { Page } from "@/types/Page";
 
 import { authOptions } from "./api/auth/[...nextauth]";
 
-type CreateWishlistForm = {
-  name: string;
-};
-
 const HomePage: Page = () => {
   const { data: session } = useSession();
 
   const wishlists = trpc.useQuery(["wishlists.get-all"]);
-
   const createWishlist = trpc.useMutation(["wishlists.create"]);
 
-  const { handleSubmit, register, reset } = useForm<CreateWishlistForm>();
+  const { openNewWishlistModal } = useNewWishlistModal();
 
   const onSubmit = async (data: CreateWishlistForm) => {
-    await createWishlist.mutateAsync({ name: data.name });
+    await createWishlist.mutateAsync(data);
     await wishlists.refetch();
-    reset();
   };
 
   return (
     <div>
-      {session && (
+      <NewWishlistModal onSubmit={onSubmit} />
+
+      <div className="flex justify-between items-center">
         <h2 className="text-2xl font-medium">
-          Привет, {session.user?.username}
+          Привет, {session?.user?.username}
         </h2>
-      )}
-
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-2 items-start mt-4"
-      >
-        <Input
-          type="text"
-          placeholder="Awesome wishlist name"
-          {...register("name")}
-        />
-        <Button type="submit">Создать</Button>
-      </form>
-
+        <Button onClick={() => openNewWishlistModal()}>Создать вишлист</Button>
+      </div>
       <div className="mt-4 flex flex-wrap gap-4">
         {wishlists.data?.map((wishlist) => (
           <WishlistCard
