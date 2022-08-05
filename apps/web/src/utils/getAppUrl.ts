59601cff27d07673596f817707dedd70d1c45fc8
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-08-05 22:06:23 +0300

    feat(utils): add more options for app url

--- apps/web/src/utils/getAppUrl.ts
+++ apps/web/src/utils/getAppUrl.ts
@@ -1,7 +1,17 @@
 export const getAppUrl = () => {
-  const url = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}/api/trpc/`
-    : "http://localhost:3000/api/trpc";
+  let url = "http://localhost:3000/api/trpc";
+
+  if (process.env.VERCEL_URL) {
+    url = `${process.env.VERCEL_URL}/api/trpc`;
+  }
+
+  if (process.env.NEXTAUTH_URL) {
+    url = `${process.env.NEXTAUTH_URL}/api/trpc`;
+  }
+
+  if (process.env.NEXT_PUBLIC_APP_URL) {
+    url = `${process.env.NEXT_PUBLIC_APP_URL}/api/trpc`;
+  }
 
   return url;
 };
