2b34c41da03619588e993693246b6da3afe0c922
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-08-05 20:27:53 +0300

    feat: create `user` public profile page

---
+++ apps/web/src/pages/[username]/index.tsx
@@ -0,0 +1,125 @@
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { Panel, UserAvatar } from "@wishlify/ui";
+
+import { User, Wishlist } from "@prisma/client";
+import { unstable_getServerSession } from "next-auth";
+
+import { getTRPCClient } from "@/utils/getTRPCClient";
+import { trpc } from "@/utils/trpc";
+
+import { WishlistCard } from "@/components/WishlistCard";
+import { WishlistCardList } from "@/components/WishlistCardList";
+import { DashboardLayout } from "@/layouts/DashboardLayout";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+import { Page } from "@/types/Page";
+
+type ProfilePageParams = {
+  username: string;
+};
+
+type ProfilePageProps = {
+  user: User;
+  wishlists: Array<Wishlist>;
+};
+
+const ProfilePage: Page<ProfilePageProps> = ({
+  user,
+  wishlists: initialWishlists,
+}) => {
+  const { data: wishlists } = trpc.useQuery(
+    ["wishlists.findByOwner", { userId: user?.id }],
+    { initialData: initialWishlists }
+  );
+
+  return (
+    <div>
+      <Head>
+        <title>Wishlify | {user?.username}</title>
+      </Head>
+
+      <div className="flex flex-col sm:flex-row gap-10">
+        <Panel
+          size="unsized"
+          className="sticky z-sticky top-0 bottom-0 py-5 px-10 rounded-xl shadow flex flex-col items-center gap-3"
+        >
+          {user && (
+            <UserAvatar
+              src={user.avatar}
+              fallback={user.username?.[0] || user.email[0]}
+              size={100}
+              fallbackClassName="text-4xl"
+            />
+          )}
+          <h2 className="text-lg font-medium">{user?.username}</h2>
+        </Panel>
+        <div>
+          <WishlistCardList count={wishlists?.length ?? 0}>
+            {wishlists?.map((wishlist) => (
+              <Link
+                key={wishlist.id}
+                href={`/${user.username}/${wishlist.displayName}`}
+                passHref
+              >
+                <a className="block">
+                  <WishlistCard wishlist={wishlist} className="h-full" />
+                </a>
+              </Link>
+            ))}
+          </WishlistCardList>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProfilePage.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export const getServerSideProps: GetServerSideProps<
+  ProfilePageProps,
+  ProfilePageParams
+> = async (ctx) => {
+  const { username } = ctx.params || {};
+
+  if (!username) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const session = await unstable_getServerSession(
+    ctx.req,
+    ctx.res,
+    authOptions
+  );
+
+  const client = getTRPCClient();
+
+  const user = await client.query("user.findByUsername", { username });
+  if (!user) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const wishlists = await client.query("wishlists.findByOwner", {
+    userId: user.id,
+  });
+
+  return {
+    props: {
+      session,
+      user,
+      wishlists,
+    },
+  };
+};
+
+export default ProfilePage;
--- apps/web/src/server/routers/users.ts
+++ apps/web/src/server/routers/users.ts
@@ -6,33 +6,49 @@ import { createRouter } from "@/utils/router";
 
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
-export const userRouter = createRouter().mutation("update-username", {
-  input: z.object({
-    username: z.string(),
-  }),
-  async resolve({ input, ctx }) {
-    const { req, res, prisma } = ctx;
-
-    const usernameTaken = await prisma.user.findFirst({
-      where: { username: input.username },
-    });
-
-    if (usernameTaken) {
-      throw new trpc.TRPCError({
-        code: "BAD_REQUEST",
-        message: "Username already taken",
+export const userRouter = createRouter()
+  .query("findByUsername", {
+    input: z.object({
+      username: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const user = await ctx.prisma.user.findUnique({
+        where: { username: input.username },
       });
-    }
+      if (!user) {
+        return null;
+      }
 
-    const session = await unstable_getServerSession(req, res, authOptions);
+      return user;
+    },
+  })
+  .mutation("update-username", {
+    input: z.object({
+      username: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const { req, res, prisma } = ctx;
 
-    const updatedUser = await prisma.user.update({
-      data: { username: input.username },
-      where: { id: session?.user?.id },
-    });
+      const usernameTaken = await prisma.user.findFirst({
+        where: { username: input.username },
+      });
 
-    return {
-      user: updatedUser,
-    };
-  },
-});
+      if (usernameTaken) {
+        throw new trpc.TRPCError({
+          code: "BAD_REQUEST",
+          message: "Username already taken",
+        });
+      }
+
+      const session = await unstable_getServerSession(req, res, authOptions);
+
+      const updatedUser = await prisma.user.update({
+        data: { username: input.username },
+        where: { id: session?.user?.id },
+      });
+
+      return {
+        user: updatedUser,
+      };
+    },
+  });
