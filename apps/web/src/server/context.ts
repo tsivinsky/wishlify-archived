d9593c045afb547f2d4131c6032d969a4aaee604
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-08-05 20:23:27 +0300

    refactor: init prisma client in `createContext` function

--- apps/web/src/server/context.ts
+++ apps/web/src/server/context.ts
@@ -1,13 +1,14 @@
+import { PrismaClient } from "@prisma/client";
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { unstable_getServerSession } from "next-auth";
-
-import { prisma } from "@/utils/prisma";
 
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export async function createContext(ctx: trpcNext.CreateNextContextOptions) {
   const { req, res } = ctx;
+
+  const prisma = new PrismaClient();
 
   const session = await unstable_getServerSession(req, res, authOptions);
 
