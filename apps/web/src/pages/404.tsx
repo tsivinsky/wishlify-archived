055231a765e0ad2ebcedb52c65d9ce0535fbd432
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-08-08 23:49:07 +0300

    feat: add `404` page

---
+++ apps/web/src/pages/404.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+import { Button } from "@wishlify/ui";
+
+const NotFoundPage = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-around items-center bg-gray-50 dark:bg-primary-dark dark:text-white/90">
+      <div />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-6xl md:text-[256px] text-center">404</h1>
+        <h2 className="text-lg md:text-3xl text-center">
+          Не знаю что вы искали, но этого здесь нет.
+        </h2>
+      </div>
+      <Link href="/" passHref>
+        <Button as="a" color="gray" size="large" className="mt-5">
+          Домой
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
