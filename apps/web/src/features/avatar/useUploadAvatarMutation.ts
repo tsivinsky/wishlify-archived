b8a586f230708cbdf6d41e61ec25cf367f8636f0
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-09-11 20:39:20 +0300

    refactor: move file api to features dir

--- apps/web/src/features/avatar/useUploadAvatarMutation.ts
+++ apps/web/src/features/avatar/useUploadAvatarMutation.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 
-import { uploadAvatar } from "@/api/file";
+import { uploadAvatar } from "./api";
 
 export const useUploadAvatarMutation = () => {
   return useMutation((file: File) => uploadAvatar<{ image?: string }>(file));
